03ea0ce987e2914b7e9f84c4c00568f10bca4e8a
Josefina <134737124+Monroestein@users.noreply.github.com>
2024-08-14 19:28:24 -0400

    added a bit of style

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+//Crear una clase Producto con los atributos 'nombre' y 'precio'
+
+function Product(name, price){
+    this.name = name;
+    this.price = price;
+};
+
+let product1 = new Product('Green Shell', 1000);
+let product2 = new Product('Blue Shell', 2000);
+let product3 = new Product('Bullet Bill', 1200);
+let product4 = new Product('Bananas', 890);
+let product5 = new Product('Mushroom', 1300);
+
+//
+
