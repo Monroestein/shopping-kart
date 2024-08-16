edf3129fdfd82cbe28281f6782ff7c18135bfc87
Josefina <134737124+Monroestein@users.noreply.github.com>
2024-08-16 17:47:47 -0400

    so far so good

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,18 +3,61 @@
 let startButton = document.getElementById('start_button');
 startButton.addEventListener('click', welcoming)
 
+let kart = new Kart()
+let init = false
+
 function welcoming(){
-    alert(`Welcome to Shopping Kart ⚡`);
-    addingItems()
+    if(init==false){
+        alert(`Welcome to Shopping Kart ⚡`);
+        init = true;
+    }
+    let indexProduct = addingItems()
+    let productQuantity = addingQuantity();
+    console.log(products[indexProduct-1])
+
+    let productAdd = products[indexProduct-1]
+    productAdd.quantity = productQuantity;
+    console.log(productAdd)
+
+    kart.addProduct(productAdd);
+    // console.log(kart.getTotal())
+
+    let finish = askFinish();
+    if (finish == 'y'){
+        //boleta (detalle - total)
+        let ticket = document.getElementById('ticket');
+        ticket.innerHTML=`<h3>
+        <br>
+        <p>El total de su compra es: $${kart.getTotal()}.</p></h3>`
+
+    }
+}
+
+function askFinish(){
+    let response = prompt(`Finish shopping? [Y/N]`).toLowerCase();
+    console.log(response)
+    if (response == 'y' || response == 'n'){
+        
+        return response
+    }
+    alert(`Invalid option.`)
 }
 
 //Crear una clase Producto con los atributos 'nombre' y 'precio'
 
 let product1 = new Product('Green Shell', 1000);
 let product2 = new Product('Blue Shell', 5000);
-let product3 = new Product('Bullet Bill', 1200);
+let product3 = new Product('Bullet Bill', 1500);
 let product4 = new Product('Bananas', 890);
 let product5 = new Product('Mushroom', 1300);
+
+let products = [
+    product1,
+    product2,
+    product3,
+    product4,
+    product5
+]
 
 function Product(name, price){
     let _name = name;
@@ -32,16 +75,32 @@ function Product(name, price){
 
 // Crea la clase Carrito que tenga como atributo un arreglo para almacenar los productos seleccionados.
 
-function Kart(selectedItem){
+function Kart(){
     let _selectedItem = [];
 
     Object.defineProperties(this,{
         selectedItem:{
+            get:function(){
+                return _selectedItem
+            },
             set:function(new_selectedItem){
                  _selectedItem = new_selectedItem}
         }
     })
+
+    this.getTotal = function(){
+        return this.selectedItem.reduce((total, product) => {
+            return total + (product.price * product.quantity)
+        },0)
+    }
+
+    this.addProduct = function(product){
+        this.selectedItem.push(product)
+    }
+
 };
+
+let productAdded = new Kart();
 
 
 
@@ -49,8 +108,21 @@ function Kart(selectedItem){
 
 function addingItems(){
     let addItem = prompt(`Which product would you like to purchase?`);
-    return addItem;
+    if (addItem > 0 && addItem < 6){
+        return addItem
+    }
+    else {
+        alert(`Please select a valid number.`)
+    }
+    return
 }
 
-
+function addingQuantity(){
+    let quantity = parseInt(prompt(`How many would you like to buy?`));
+    if (isNaN(quantity)){
+        alert(`Please select a valid number.`)
+        return
+    }
+    return quantity
+}
 
